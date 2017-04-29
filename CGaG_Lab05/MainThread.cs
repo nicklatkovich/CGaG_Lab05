a9b356d6524189e64b776ce1ae8d1ed8de095b00
NickLatkovich <nicklatkovich@gmail.com>
2017-04-29 12:52:44 +0300

    Drawing Primitives

--- CGaG_Lab05/MainThread.cs
+++ CGaG_Lab05/MainThread.cs
@@ -7,17 +7,43 @@ namespace CGaG_Lab05 {
         GraphicsDeviceManager Graphics;
         SpriteBatch SpriteBatch;
 
+        Matrix WorldMatrix;
+        Matrix ViewMatrix;
+        Matrix ProjectionMatrix;
+        BasicEffect Effect;
+
+        float AxesLight = 0.4f;
+
+        Color[ ] AxesColors;
+
         Color BackColor = new Color(30, 30, 30);
 
         public MainThread( ) {
+            AxesColors = new Color[3] {
+                new Color(AxesLight, 0f, 0f),
+                new Color(0f, AxesLight, 0f),
+                new Color(0f, 0f, AxesLight),
+            };
             Graphics = new GraphicsDeviceManager(this);
             Window.Title = "CGaG Lab 5 by NickLatkovich";
             base.IsMouseVisible = true;
             Content.RootDirectory = "Content";
+
         }
 
         protected override void Initialize( ) {
             // TODO: Initialization logic
+            WorldMatrix = Matrix.Identity;
+            ViewMatrix = Matrix.CreateLookAt(new Vector3(10f, 10f, 10f), Vector3.Zero, Vector3.Up);
+
+            ProjectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), GraphicsDevice.Viewport.Width / GraphicsDevice.Viewport.Height, 1.0f, 100.0f);
+
+            Effect = new BasicEffect(Graphics.GraphicsDevice);
+
+            Effect.World = WorldMatrix;
+            Effect.View = ViewMatrix;
+            Effect.Projection = ProjectionMatrix;
+            Effect.VertexColorEnabled = true;
 
             base.Initialize( );
         }
@@ -45,6 +71,21 @@ namespace CGaG_Lab05 {
             GraphicsDevice.Clear(this.BackColor);
 
             // TODO: Drawing code
+            RasterizerState rasterizerState1 = new RasterizerState( );
+            rasterizerState1.CullMode = CullMode.None;
+            Graphics.GraphicsDevice.RasterizerState = rasterizerState1;
+            foreach (EffectPass pass in Effect.CurrentTechnique.Passes) {
+                pass.Apply( );
+                this.DrawLineList(new VertexPositionColor[6] {
+                    new VertexPositionColor(new Vector3(-1024f, 0f, 0f), AxesColors[0]),
+                    new VertexPositionColor(new Vector3(1024f, 0f, 0f), AxesColors[0]),
+                    new VertexPositionColor(new Vector3(0f, 1024f, 0f), AxesColors[1]),
+                    new VertexPositionColor(new Vector3(0f, -1024f, 0f), AxesColors[1]),
+                    new VertexPositionColor(new Vector3(0f, 0f, 1024f), AxesColors[2]),
+                    new VertexPositionColor(new Vector3(0f, 0f, -1024f), AxesColors[2]),
+                });
+                this.DrawLineTriangle(new Vector3(0f, -2f, 0f), new Vector3(2f, 1f, 0f), new Vector3(-2f, 1f, 0f), Color.Blue);
+            }
 
             base.Draw(Time);
         }
---
+++ CGaG_Lab05/SimpleUtils.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGaG_Lab05 {
+    public static class SimpleUtils {
+
+        public static void DrawLineTriangle(this Game thread, Vector3 pos1, Vector3 pos2, Vector3 pos3, Color color) {
+            VertexPositionColor[ ] points = new VertexPositionColor[3];
+            points[0] = new VertexPositionColor(pos1, color);
+            points[1] = new VertexPositionColor(pos2, color);
+            points[2] = new VertexPositionColor(pos3, color);
+            thread.DrawLineStrip(points, true);
+        }
+
+        public static void DrawLineList(this Game thread, VertexPositionColor[ ] vertexList) {
+            short[ ] indices = new short[vertexList.Length];
+            for (short i = 0; i < vertexList.Length; i++) {
+                indices[i] = i;
+            }
+
+            VertexBuffer vertexBuffer = new VertexBuffer(thread.GraphicsDevice, typeof(VertexPositionColor), vertexList.Length, BufferUsage.WriteOnly);
+            vertexBuffer.SetData(vertexList);
+            thread.GraphicsDevice.SetVertexBuffer(vertexBuffer);
+
+            thread.GraphicsDevice.DrawPrimitives(PrimitiveType.LineList, 0, vertexList.Length / 2);
+        }
+
+        public static void DrawLineStrip(this Game thread, VertexPositionColor[ ] vertexList, bool isLooped = false) {
+            short[ ] indices = new short[vertexList.Length + (isLooped ? 1 : 0)];
+            for (short i = 0; i < vertexList.Length; i++) {
+                indices[i] = i;
+            }
+            if (isLooped) {
+                indices[vertexList.Length] = 0;
+            }
+
+            thread.GraphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.LineStrip, vertexList, 0, vertexList.Length, indices, 0, indices.Length - 1);
+        }
+
+    }
+}
